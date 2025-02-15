fe4601dbb2a0c74ea0ab82a9580f9d2e7f5e680c
thunder <thunder@unknown.com>
2025-02-15 16:57:20 +0800

    little fixed

--- crawl/driver.py
+++ crawl/driver.py
@@ -1,8 +1,5 @@
 import time
 from seleniumwire import webdriver
-from seleniumwire import utils
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 
 class EdgeDriver(webdriver.Edge):
